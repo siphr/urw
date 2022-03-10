0110d11165d1fbb5129c5e2e5cb34ce757b76e57
siphr <haris@sscriptureofman.com>
2022-03-10 19:44:16 +0000

    init pip

---
+++ pip/README.md
@@ -0,0 +1,19 @@
+URW
+===
+
+A random Urdu word generator.
+
+
+Details
+-------
+
+Uses a predefined set of words for enumeration.
+
+
+Package Usage
+-------------
+
+import urw
+
+\# show a phrase made of n=2 random words
+urw.show_random_words(2)
---
+++ pip/urw.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import sys
+import getopt
+import requests as r
+import random
+
+
+def show_random_words(number_of_words):
+    f = open('wordlist.txt', 'r', encoding='utf-16')
+    lines = f.readlines()
+
+    random_res = ''
+    while number_of_words > 0:
+        random_res += random.choice(lines).strip() + ' '
+        number_of_words = number_of_words - 1
+        
+    return random_res.strip()
+
+if __name__ == '__main__':
+    opts, args = getopt.getopt(sys.argv[1:], 'n:h',['number_of_words', 'help'])
+    for o, a in opts:
+        if o in ['-h', '--help']:
+            show_help()
+            sys.exit(0)
+        elif o in ['-n', '--number_of_words']:
+            ans = show_random_words(int(a))
+
+
+    print('\033[1mRESULT\033[0m {}'.format(ans))
